f8a3e2f4c7ed6c09d2456cdf90a6d6f658da328d
Fairytale-DEF <farid.djerrar@efrei.net>
2025-02-20 14:33:21 +0100

    images OK

--- app/(pages)/api-images/page.jsx
+++ app/(pages)/api-images/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Loading from "@/app/loading";
 import Title from "@/components/title/Title";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Api_Images() {
@@ -18,6 +20,7 @@ export default function Api_Images() {
       }
       const data = await response.json();
       console.log(data);
+      setImages(data);
     } catch (error) {
       console.error("Error fetching images :", error);
       setError(error);
@@ -33,6 +36,26 @@ export default function Api_Images() {
   return (
     <div>
       <Title>Les images de l'API :</Title>
+      {/* Loader si chargement en cours */}
+      {loading && <Loading />}
+      {/* {si erreur} */}
+      {error && <p className="text-red-500 text-center">{error}</p>}
+
+      <div className="flex items-center justify-center flex-wrap gap-4">
+        {images.map((image, index) => (
+          <div key={index}>
+            <Image
+              src={image.url}
+              alt={image.name}
+              width={150}
+              height={150}
+              className="h-auto object-cover rounded-lg w-64"
+              // unoptimized={true}
+            />
+            <p>{image.name} </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ app/Loading.jsx
@@ -0,0 +1,7 @@
+import { TfiReload } from "react-icons/tfi";
+
+export default function Loading() {
+  return (
+    <TfiReload className="mx-auto font-bold text-purple-500 text-7xl animate-spin scale-[-1]" />
+  );
+}
